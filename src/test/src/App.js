bd76b66347db3438e5c8869c02444402adf7e00f
cnawarathne <cnawarathne@sentineladvantage.com>
2024-03-04 16:51:40 -0500

    add test project

---
+++ src/test/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@cnawarathne/shared-component-synergy-poc";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>sfsfdsf</Button>
+    </div>
+  );
+}
+
+export default App;
